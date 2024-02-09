081fdd57b479266df2d187e97eb432d330492a60
saibushifu <proqxt@proton.me>
2024-02-10 01:03:35 +0300

    .

---
+++ AnimeJail.App/Methods/LoginData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeJail.App.Methods
+{
+   public class LoginData
+    {
+        private string _username;
+        private string _password;
+        public LoginData(string username, string password) {
+            _username = username;
+            _password = password;
+        }
+
+        public bool TryLogin() { 
+            if (true)
+            {
+               return CheckPassword();
+            }
+            return true; 
+        }
+        private bool CheckPassword() { 
+            return false; 
+        }
+    }
+}
